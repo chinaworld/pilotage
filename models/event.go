fdb7c1542c8128671277b59e480f0a4dce29c4c6
Meaglith Ma <genedna@gmail.com>
2016-08-12 04:33:56 -0400

    Add source property in Event model

--- models/event.go
+++ models/event.go
@@ -5,8 +5,10 @@ import (
 )
 
 const (
-	SYSTEM_EVENT = iota
-	USER_EVENT
+	TYPE_SYSTEM_EVENT = iota
+	TYPE_USER_EVENT
+	SOURCE_INNER_EVENT
+	SOURCE_OUT_EVENT
 )
 
 //Event is
@@ -14,7 +16,8 @@ type Event struct {
 	ID          int64      `json:"id" gorm:"primary_key"`
 	ComponentID int64      `json:"component_id" sql:"not null" gorm:"unique_index:component_event"`
 	Event       string     `json:"event" sql:"not null;type:varchar(255)" gorm:"unique_index:component_event"`
-	Type        int64      `json:"type" sql:"not null"`
+	Type        int64      `json:"type" sql:"not null;type:varchar(255)"`
+	Source      int64      `json:"source" sql:"not null;type:varchar(255)"`
 	Definition  string     `json:"type" sql:"null;type:text"`
 	CreatedAt   time.Time  `json:"created" sql:""`
 	UpdatedAt   time.Time  `json:"updated" sql:""`
